6969e3c3277f2f53e9c45542e1130a50f2938e8b
cp-20 <n.naoki.1587@gmail.com>
2024-09-16 00:07:54 -0700

    バグ直し

--- packages/lb-server/src/index.ts
+++ packages/lb-server/src/index.ts
@@ -1,106 +1,124 @@
-import { Hono } from 'hono';
+import { Hono } from "hono";
 import {
   createSubmission,
   createUser,
   getRanking,
   getTimeline,
-  getUser
-} from './database/repository';
-import { z } from 'zod';
-import { zValidator } from '@hono/zod-validator';
+  getUser,
+} from "./database/repository";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 
 const secretKey = process.env.SECRET_KEY;
 if (secretKey === undefined) {
-  throw new Error('環境変数 SECRET_KEY が設定されていません');
+  throw new Error("環境変数 SECRET_KEY が設定されていません");
 }
+
+const headerKeyName = "x-secret-key";
 
 const app = new Hono();
 
 const submissionSchema = z.object({
   name: z.string(),
-  score: z.number()
+  score: z.number(),
 });
 
 const createUserSchema = z.object({
   name: z.string(),
-  url: z.string().url()
+  url: z.string().url(),
 });
 
 const route = app
-  .get('/ranking', async (c) => {
+  .get("/ranking", async (c) => {
     try {
       const result = await getRanking();
       return c.json(result);
     } catch (err) {
       console.error(err);
-      return c.json({ message: 'Internal server error' }, 500);
+      return c.json({ message: "Internal server error" }, 500);
     }
   })
-  .get('/timeline', zValidator('query', z.object({ name: z.array(z.string()) })), async (c) => {
-    const names = c.req.valid('query').name;
-    if (names.length === 0) {
-      return c.json({ message: 'userId is required' }, 400);
-    }
-
-    try {
-      const result = await getTimeline(names);
-      return c.json(result);
-    } catch (err) {
-      console.error(err);
-      return c.json({ message: 'Internal server error' }, 500);
-    }
-  })
-  .post(
-    '/submissions',
-    zValidator('header', z.object({ 'X-Secret-Key': z.enum([secretKey]) })),
-    zValidator('json', submissionSchema),
+  .get(
+    "/timeline",
+    zValidator("query", z.object({ name: z.array(z.string()) })),
     async (c) => {
-      const body = c.req.valid('json');
+      const names = c.req.valid("query").name;
+      if (names.length === 0) {
+        return c.json({ message: "userId is required" }, 400);
+      }
+
+      try {
+        const result = await getTimeline(names);
+        return c.json(result);
+      } catch (err) {
+        console.error(err);
+        return c.json({ message: "Internal server error" }, 500);
+      }
+    },
+  )
+  .post(
+    "/submissions",
+    zValidator("json", submissionSchema),
+    zValidator("header", z.object({ [headerKeyName]: z.string() })),
+    async (c) => {
+      if (c.req.valid("header")[headerKeyName] !== secretKey) {
+        return c.json({ message: "Invalid secret key" }, 403);
+      }
+
+      const body = c.req.valid("json");
       try {
         const { name, score } = body;
         await createSubmission({ name, score });
-        return c.json({ message: 'Submission created' });
+        return c.json({ message: "Submission created" });
       } catch (err) {
         console.error(err);
-        return c.json({ message: 'Internal server error' }, 500);
+        return c.json({ message: "Internal server error" }, 500);
       }
-    }
+    },
   )
   .post(
-    '/users',
-    zValidator('header', z.object({ 'X-Secret-Key': z.enum([secretKey]) })),
-    zValidator('json', createUserSchema),
+    "/users",
+    zValidator("json", createUserSchema),
+    zValidator("header", z.object({ [headerKeyName]: z.string() })),
     async (c) => {
-      const body = c.req.valid('json');
+      if (c.req.valid("header")[headerKeyName] !== secretKey) {
+        return c.json({ message: "Invalid secret key" }, 403);
+      }
+
+      const body = c.req.valid("json");
 
       try {
         const { name, url } = body;
         await createUser({ name, url });
-        return c.json({ message: 'User created' });
+        return c.json({ message: "User created" });
       } catch (err) {
         console.error(err);
-        return c.json({ message: 'Internal server error' }, 500);
+        return c.json({ message: "Internal server error" }, 500);
       }
-    }
+    },
   )
-  .get('/users/:id', zValidator('param', z.object({ id: z.string() })), async (c) => {
-    const userId = c.req.valid('param').id;
+  .get(
+    "/users/:id",
+    zValidator("param", z.object({ id: z.string() })),
+    async (c) => {
+      const userId = c.req.valid("param").id;
 
-    try {
-      const result = await getUser(userId);
-      if (result === undefined) {
-        return c.json({ message: 'Not found' }, 404);
+      try {
+        const result = await getUser(userId);
+        if (result === undefined) {
+          return c.json({ message: "Not found" }, 404);
+        }
+        return c.json(result);
+      } catch (err) {
+        console.error(err);
+        return c.json({ message: "Internal server error" }, 500);
       }
-      return c.json(result);
-    } catch (err) {
-      console.error(err);
-      return c.json({ message: 'Internal server error' }, 500);
-    }
-  });
+    },
+  );
 
 export default {
   port: 3000,
-  fetch: app.fetch
+  fetch: app.fetch,
 };
 
 export type AppType = typeof route;
