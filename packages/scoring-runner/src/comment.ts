072e3b62d36b78ee593e68d0fa70b78e90d82ddb
cp-20 <n.naoki.1587@gmail.com>
2024-09-18 15:17:01 +0900

    format

--- packages/scoring-runner/src/comment.ts
+++ packages/scoring-runner/src/comment.ts
@@ -1,41 +1,38 @@
-import { context, getOctokit } from "@actions/github";
+import { context, getOctokit } from '@actions/github';
 
 const token = process.env.GITHUB_TOKEN;
 if (token === undefined) {
-  throw new Error("環境変数 GITHUB_TOKEN が設定されていません");
+  throw new Error('環境変数 GITHUB_TOKEN が設定されていません');
 }
 const octokit = getOctokit(token);
 
 const contextCommentId = context.payload.comment?.id;
 
 export const isRegister = async () => {
-  return context.eventName === "issues" && context.payload.action === "opened";
+  return context.eventName === 'issues' && context.payload.action === 'opened';
 };
 
 export const isRetry = async () => {
   const comment = await getContextComment();
   if (comment === undefined) return false;
   if (comment.user !== context.issue.owner) return false;
-  if (comment.body.trim() !== "/retry") return false;
+  if (comment.body.trim() !== '/retry') return false;
   return true;
 };
 
 export const getContextIssue = async () => {
-  const { data: issue } = await octokit.request(
-    "GET /repos/{owner}/{repo}/issues/{issue_number}",
-    {
-      owner: context.issue.owner,
-      repo: context.issue.repo,
-      issue_number: context.issue.number,
-    },
-  );
+  const { data: issue } = await octokit.request('GET /repos/{owner}/{repo}/issues/{issue_number}', {
+    owner: context.issue.owner,
+    repo: context.issue.repo,
+    issue_number: context.issue.number
+  });
   if (issue.body === undefined || issue.body === null) {
-    throw new Error("issue.body is undefined or null");
+    throw new Error('issue.body is undefined or null');
   }
 
   return {
     body: issue.body,
-    user: issue.user?.login,
+    user: issue.user?.login
   };
 };
 
@@ -43,61 +40,52 @@ export const getContextComment = async () => {
   if (contextCommentId === undefined) return undefined;
 
   const { data: comment } = await octokit.request(
-    "GET /repos/{owner}/{repo}/issues/comments/{comment_id}",
+    'GET /repos/{owner}/{repo}/issues/comments/{comment_id}',
     {
       owner: context.issue.owner,
       repo: context.issue.repo,
-      comment_id: contextCommentId,
-    },
+      comment_id: contextCommentId
+    }
   );
   if (comment.body === undefined) {
-    throw new Error("comment.body is undefined");
+    throw new Error('comment.body is undefined');
   }
 
   return {
     body: comment.body,
-    user: comment.user?.login,
+    user: comment.user?.login
   };
 };
 
 export const replyReaction = async () => {
   if (contextCommentId === undefined) {
-    throw new Error("contextCommentId is undefined");
+    throw new Error('contextCommentId is undefined');
   }
 
-  await octokit.request(
-    "POST /repos/{owner}/{repo}/comments/{comment_id}/reactions",
-    {
-      owner: context.issue.owner,
-      repo: context.issue.repo,
-      comment_id: contextCommentId,
-      content: "+1",
-    },
-  );
+  await octokit.request('POST /repos/{owner}/{repo}/comments/{comment_id}/reactions', {
+    owner: context.issue.owner,
+    repo: context.issue.repo,
+    comment_id: contextCommentId,
+    content: '+1'
+  });
 };
 
 let replyCommentId = null;
 
 export const updateComment = async (comment: string) => {
   if (replyCommentId === null) {
-    await octokit.request(
-      "POST /repos/{owner}/{repo}/issues/{issue_number}/comments",
-      {
-        owner: context.issue.owner,
-        repo: context.issue.repo,
-        issue_number: context.issue.number,
-        body: comment,
-      },
-    );
+    await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+      owner: context.issue.owner,
+      repo: context.issue.repo,
+      issue_number: context.issue.number,
+      body: comment
+    });
   } else {
-    await octokit.request(
-      "PATCH /repos/{owner}/{repo}/issues/comments/{comment_id}",
-      {
-        owner: context.issue.owner,
-        repo: context.issue.repo,
-        comment_id: replyCommentId,
-        body: comment,
-      },
-    );
+    await octokit.request('PATCH /repos/{owner}/{repo}/issues/comments/{comment_id}', {
+      owner: context.issue.owner,
+      repo: context.issue.repo,
+      comment_id: replyCommentId,
+      body: comment
+    });
   }
 };
--- packages/scoring-runner/src/main.ts
+++ packages/scoring-runner/src/main.ts
@@ -1,24 +1,16 @@
-import { createUser, createUserSubmission, fetchUserUrl } from "./gateway";
-import {
-  getContextComment,
-  getContextIssue,
-  isRegister,
-  isRetry,
-  replyReaction,
-} from "./comment";
-import { measure } from "./scoring/2024";
+import { createUser, createUserSubmission, fetchUserUrl } from './gateway';
+import { getContextComment, getContextIssue, isRegister, isRetry, replyReaction } from './comment';
+import { measure } from './scoring/2024';
 
 if (await isRegister()) {
   const issue = await getContextIssue();
   const name = issue.user;
   if (name === undefined) {
-    throw new Error("name is undefined");
+    throw new Error('name is undefined');
   }
-  const url = issue.body.match(
-    /### 計測対象の URL \{\{url\}\}\n\n([a-zA-Z:\/-_]+)$/,
-  )?.[1];
+  const url = issue.body.match(/### 計測対象の URL \{\{url\}\}\n\n([a-zA-Z:\/-_]+)$/)?.[1];
   if (url === undefined) {
-    throw new Error("url is undefined");
+    throw new Error('url is undefined');
   }
   await createUser(name, url);
   await replyReaction();
@@ -29,15 +21,15 @@ if (await isRegister()) {
 if (await isRetry()) {
   const comment = await getContextComment();
   if (comment === undefined) {
-    throw new Error("comment is undefined");
+    throw new Error('comment is undefined');
   }
   const name = comment.user;
   if (name === undefined) {
-    throw new Error("name is undefined");
+    throw new Error('name is undefined');
   }
   const url = await fetchUserUrl(name);
   if (url === undefined) {
-    throw new Error("url is undefined");
+    throw new Error('url is undefined');
   }
   await replyReaction();
   const score = await measure(url);
