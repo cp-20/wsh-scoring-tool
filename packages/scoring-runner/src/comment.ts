413e40b9a91ec805d4669cb2f65c8f4832c793b1
cp-20 <n.naoki.1587@gmail.com>
2024-09-18 15:01:49 +0900

    fix runner

--- packages/scoring-runner/src/comment.ts
+++ packages/scoring-runner/src/comment.ts
@@ -1,69 +1,81 @@
-import { getInput } from '@actions/core';
-import { context, getOctokit } from '@actions/github';
+import { getInput } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
 
-const token = getInput('token');
+const token = process.env.GITHUB_TOKEN;
+if (token === undefined) {
+  throw new Error("環境変数 GITHUB_TOKEN が設定されていません");
+}
 const octokit = getOctokit(token);
 
 const contextCommentId = context.payload.comment?.id;
 if (contextCommentId === undefined) {
-  throw new Error('contextCommentId is undefined');
+  throw new Error("contextCommentId is undefined");
 }
 
 export const isRegister = async () => {
-  return context.eventName === 'issues' && context.payload.action === 'opened';
+  return context.eventName === "issues" && context.payload.action === "opened";
 };
 
 export const isRetry = async () => {
   const comment = await getContextComment();
   if (comment.user !== context.issue.owner) return false;
-  if (comment.body.trim() !== '/retry') return false;
+  if (comment.body.trim() !== "/retry") return false;
   return true;
 };
 
 export const getContextComment = async () => {
   const { data: comment } = await octokit.request(
-    'GET /repos/{owner}/{repo}/issues/comments/{comment_id}',
+    "GET /repos/{owner}/{repo}/issues/comments/{comment_id}",
     {
       owner: context.issue.owner,
       repo: context.issue.repo,
-      comment_id: contextCommentId
-    }
+      comment_id: contextCommentId,
+    },
   );
   if (comment.body === undefined) {
-    throw new Error('comment.body is undefined');
+    throw new Error("comment.body is undefined");
   }
 
   return {
     body: comment.body,
-    user: comment.user?.login
+    user: comment.user?.login,
   };
 };
 
 export const replyReaction = async () => {
-  await octokit.request('POST /repos/{owner}/{repo}/comments/{comment_id}/reactions', {
-    owner: context.issue.owner,
-    repo: context.issue.repo,
-    comment_id: contextCommentId,
-    content: '+1'
-  });
+  await octokit.request(
+    "POST /repos/{owner}/{repo}/comments/{comment_id}/reactions",
+    {
+      owner: context.issue.owner,
+      repo: context.issue.repo,
+      comment_id: contextCommentId,
+      content: "+1",
+    },
+  );
 };
 
 let replyCommentId = null;
 
 export const updateComment = async (comment: string) => {
   if (replyCommentId === null) {
-    await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
-      owner: context.issue.owner,
-      repo: context.issue.repo,
-      issue_number: context.issue.number,
-      body: comment
-    });
+    await octokit.request(
+      "POST /repos/{owner}/{repo}/issues/{issue_number}/comments",
+      {
+        owner: context.issue.owner,
+        repo: context.issue.repo,
+        issue_number: context.issue.number,
+        body: comment,
+      },
+    );
   } else {
-    await octokit.request('PATCH /repos/{owner}/{repo}/issues/comments/{comment_id}', {
-      owner: context.issue.owner,
-      repo: context.issue.repo,
-      comment_id: replyCommentId,
-      body: comment
-    });
+    await octokit.request(
+      "PATCH /repos/{owner}/{repo}/issues/comments/{comment_id}",
+      {
+        owner: context.issue.owner,
+        repo: context.issue.repo,
+        comment_id: replyCommentId,
+        body: comment,
+      },
+    );
   }
 };
