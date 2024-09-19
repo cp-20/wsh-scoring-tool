f4424d61e40b09cbe33c11b21ea2bd36e8f7a8ab
cp-20 <n.naoki.1587@gmail.com>
2024-09-19 17:49:14 +0900

    ring color 修正

--- packages/lb-frontend/src/lib/ring-color.ts
+++ packages/lb-frontend/src/lib/ring-color.ts
@@ -2,7 +2,7 @@
 
 export const getRingColor = (score: number) => {
   if (score >= 400) return '#00cc65';
-  if (score >= 200) return '#c33300';
+  if (score >= 200) return '#ffaa35';
   return '#fa3435';
 };
 
